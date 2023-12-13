7a88c1379f44acc448421b3e4260fd8d9ebb7f75
Mark McLean <62834501+MrMacker@users.noreply.github.com>
2023-12-13 01:30:49 +0000

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,74 @@
+import type {
+  Credential,
+  IotaDocument,
+  IotaIdentityClient,
+  Jwt,
+  JwtCredentialValidator,
+  JwtPresentationValidator,
+  Resolver,
+  Status,
+  Storage,
+} from "@iota/identity-wasm/node";
+import { AliasOutput, Client } from "@iota/sdk-wasm/node";
+import { Identity } from "./abstraction/identity";
+import { PresentationRequest } from "./abstraction/presentation";
 
+export type IdentityState = {
+  client: IotaIdentityClient;
+  address: string;
+  storage: Storage;
+  document: IotaDocument;
+  mnemonic: string;
+};
+
+export type RevocationState = {
+  issuer: Identity;
+  output?: AliasOutput;
+};
+
+export type CredentialState = {
+  issuer: Identity;
+  subject?: Identity;
+  properties: { [key: string]: string };
+  verificationMethod?: string;
+  revocationStatus?: Status;
+};
+
+export type Predicate = (credential: Credential) => boolean;
+
+export type PresentationRequestOptions = {
+  predicates?: Predicate[];
+  nonce?: string;
+};
+
+export type PresentationResponseState = {
+  holder: Identity;
+  nonce?: string;
+  verificationMethod?: string;
+  credentials: Jwt[];
+};
+
+export type CreatePresentationValidationState = {
+  request?: PresentationRequest;
+  response?: Jwt;
+  resolver: Resolver;
+  presentationValidator: JwtPresentationValidator;
+  credentialValidator: JwtCredentialValidator;
+};
+
+export type PresentationValidationOptions = {
+  validated: boolean;
+};
+
+export type CoverStorageDeposit = (
+  address: string,
+  tokensRequired: bigint
+) => Promise<void>;
+
+export type IdentityWalletOptions = {
+  client?: Client;
+};
+
+export type VerifierOptions = {
+  client?: Client;
+};
