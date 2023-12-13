b5852bbd1b2477aa16588272a1e533ad298b4e40
Mark McLean <62834501+MrMacker@users.noreply.github.com>
2023-12-13 01:18:38 +0000

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,1 @@
-import { CoinType } from "@iota/sdk-wasm/node";
 
-export const NODE = "https://api.testnet.shimmer.network";
-export const FAUCET = "https://faucet.testnet.shimmer.network/api/enqueue";
-export const EXPLORER = "https://explorer.iota.org/testnet";
-export const COIN_TYPE = CoinType.Shimmer;
-export const DIRECTORY = "wallet";
